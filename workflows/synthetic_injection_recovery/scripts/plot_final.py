ce72a9e73bb501570143f61c0925636b27e3ffb2
lgrcia <lionel_garcia@live.fr>
2023-01-26 16:48:09 +0100

    paper: simulated injection recovery results

--- workflows/synthetic_injection_recovery/scripts/plot_final.py
+++ workflows/synthetic_injection_recovery/scripts/plot_final.py
@@ -7,8 +7,8 @@ from matplotlib import patches
 
 nuance_main, wotan_main, nuance_zoom, wotan_zoom = snakemake.input
 
-ylabel = r"$\delta_v \propto \frac{\mathrm{variability\; amplitude}}{\mathrm{transit\;depth}}$"
-xlabel = r"$\tau_v \propto \frac{\mathrm{variability\; timescale}}{\mathrm{transit\;duration}}$"
+ylabel = r"$\delta$"
+xlabel = r"$\tau$"
 
 fig = plt.figure(None, (7, 6.5))
 
@@ -34,10 +34,10 @@ im = main.imshow(
     vmin=0.,
     vmax=1.
 )
-main.set_title("wõtan + TLS", loc="left")
+main.set_title("wõtan + TLS\n\n")
 
 
-zoomw = plt.subplot(2, 2, 2)
+zoomw = plt.subplot(2, 2, 3)
 bins = (30,30)
 df = pd.read_csv(wotan_zoom)
 detected, _var, _amp = df.values.T.astype(float)
@@ -56,22 +56,23 @@ zoomw.imshow(
     vmax=1.
 )
 
-a = (0.5, 1) # xy of lower left box
+a = (0.01,0.01) # xy of lower left box
 b = (10, 25) # xy of upper right box
 pcolor="0.6"
 p = patches.Polygon([a, (a[0], b[1]), (b[0], b[1]), (b[0], a[1])], fill=False, ec=pcolor)
 main.add_patch(p)
 con = patches.ConnectionPatch(xyA=(a[0], b[1]), xyB=(0.1, b[1]-0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoomw, color=pcolor)
 fig.add_artist(con)
-con = patches.ConnectionPatch(xyA=(b[0], a[1]), xyB=(0.1, 0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoomw, color=pcolor)
+con = patches.ConnectionPatch(xyA=(b[0], a[1]), xyB=(b[0], b[1]-0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoomw, color=pcolor)
 fig.add_artist(con)
 zoomw.set_ylim(0.1, b[1])
 zoomw.set_xlim(0.1, b[0])
+zoomw.set_xlabel(xlabel, fontsize=14)
+zoomw.set_ylabel(ylabel, fontsize=14)
 
 # nuance
 # ------
-main = plt.subplot(2, 2, 3)
-main.set_ylabel(ylabel, fontsize=14)
+main = plt.subplot(2, 2, 2)
 bins = (30,30)
 df = pd.read_csv(nuance_main)
 detected, _var, _amp = df.values.T.astype(float)
@@ -89,7 +90,8 @@ main.imshow(
     vmin=0.,
     vmax=1.
 )
-main.set_title("nuance", loc="left")
+main.set_title("nuance\n")
+axins = main.inset_axes((0.455, 1.03, 0.4, 0.06))
 
 zoom = plt.subplot(2, 2, 4)
 df = pd.read_csv(nuance_zoom)
@@ -114,16 +116,14 @@ p = patches.Polygon([a, (a[0], b[1]), (b[0], b[1]), (b[0], a[1])], fill=False, e
 main.add_patch(p)
 con = patches.ConnectionPatch(xyA=(a[0], b[1]), xyB=(0.1, b[1]-0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoom, color=pcolor)
 fig.add_artist(con)
-con = patches.ConnectionPatch(xyA=(b[0], a[1]), xyB=(0.1, 0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoom, color=pcolor)
+con = patches.ConnectionPatch(xyA=(b[0], a[1]), xyB=(b[0], b[1]-0.1), coordsA="data", coordsB="data", axesA=main, axesB=zoom, color=pcolor)
 fig.add_artist(con)
 zoom.set_ylim(0.1, b[1])
 zoom.set_xlim(0.1, b[0])
-main.set_xlabel(xlabel, fontsize=14)
 zoom.set_xlabel(xlabel, fontsize=14)
 
 plt.tight_layout()
 
-axins = zoomw.inset_axes((0.455, 1.03, 0.4, 0.06))
 cb = fig.colorbar(im, cax=axins, orientation="horizontal", ticks=[])
 cb.ax.text(-0.05, 0.5, "0%", va="center", ha="right")
 cb.ax.text(1.06, 0.5, "100%", va="center", ha="left")
