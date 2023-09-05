a86bbff96379b56395244a325f927b81fd9836e8
lgrcia <lionel_garcia@live.fr>
2023-09-05 19:18:45 -0400

    fit: gp optimization for each injection

---
+++ workflows/real_injection_recovery_gp_opt/scripts/concatenate.py
@@ -0,0 +1,55 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+
+
+def right_candidate(t0, period, true_t0, true_period, verbose=False):
+    t0_check = (
+        np.abs((t0 - true_t0 + 0.5 * true_period) % true_period - 0.5 * true_period)
+        % period
+        < 0.01
+    )
+    period_check = np.abs(period - true_period) < 0.01
+    period_check |= np.abs(2 * period - true_period) < 0.01
+    period_check |= np.abs(period / 2 - true_period) < 0.01
+    same = period_check  # np.logical_and(t0_check, period_check)
+    if verbose:
+        if not same:
+            if not t0_check:
+                output = f"t0 differ: {t0:.2e} {true_t0:.2e}"
+            if not period_check:
+                output = f"period differ: {period:.2e} {true_period:.2e}"
+        else:
+            output = "match"
+        return same, output
+    else:
+        return same
+
+
+def get_result(injected_file, recovered_file):
+    injected = pickle.load(open(injected_file, "rb"))
+    recovered = pickle.load(open(recovered_file, "rb"))
+    t0, period = recovered["t0"], recovered["period"]
+    true_t0, true_period = injected["transit_t0"], injected["planet_period"]
+    return {
+        "t0": t0,
+        "period": period,
+        "true_t0": true_t0,
+        "true_period": true_period,
+        "found": right_candidate(t0, period, true_t0, true_period),
+        "radius": injected["planet_radius"],
+        "tau": injected["tau"],
+        "delta": injected["delta"],
+    }
+
+
+recovered = snakemake.input.recovered
+injected = snakemake.input.injected
+
+results = []
+
+for i, (a, b) in enumerate(zip(injected, recovered)):
+    results.append(get_result(a, b))
+results = pd.DataFrame(results)
+results.to_csv(snakemake.output[0], index=False)
