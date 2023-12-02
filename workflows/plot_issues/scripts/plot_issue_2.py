b3492709d9d2bcb3d3bbcb31b6ff06140638d07a
lgrcia <lionel_garcia@live.fr>
2023-12-02 18:18:16 -0500

    aal figures go in workflows

--- workflows/plot_issues/scripts/plot_issue_2.py
+++ workflows/plot_issues/scripts/plot_issue_2.py
@@ -5,10 +5,43 @@ import tinygp
 import jax
 import matplotlib.pyplot as plt
 jax.config.update("jax_enable_x64", True)
-import sys
-sys.path.append("./lib")
-sys.path.append("../lib")
-from utils import white_red
+
+import jax.numpy as jnp
+from tinygp import kernels, GaussianProcess
+import matplotlib.pyplot as plt
+
+
+def white_red(time, flux, error, plot=False, mean=0.):
+    init = {
+        "log_diag": 2*np.log(np.mean(error)),
+        "log_scale": np.log(1), # 5 mins
+        "log_amp": np.log(np.std(flux))
+    }
+
+    def build_gp(params):
+        kernel = jnp.exp(params["log_amp"]) * kernels.ExpSquared(jnp.exp(params["log_scale"]))
+        return GaussianProcess(kernel, time, diag=jnp.exp(params["log_diag"]), mean=mean)
+
+    @jax.jit
+    def loss(params):
+        gp = build_gp(params)
+        return -gp.log_probability(flux)
+    
+    opt = jaxopt.ScipyMinimize(fun=loss)
+    params = opt.run(init).params  
+    w = np.sqrt(np.exp(params['log_diag']))
+    
+    if plot:
+        gp = build_gp(params)
+        _, cond = gp.condition(y=flux, X_test=time)
+        mu = cond.mean
+        plt.plot(time, flux, ".", c="0.8")
+        plt.plot(time, mu, c="k")
+        noise = w
+        plt.fill_between(time, mu + noise, mu - noise, color="C0", alpha=0.3)
+
+    return w, np.sqrt(np.exp(params['log_amp']))
+
 
 seed = 42
 
@@ -141,4 +174,4 @@ plt.text(6.5, 1.005, f"{original_snr:.2f}", ha="center", fontsize=14, color=txt_
 
 plt.axis("off")
 plt.tight_layout()
-plt.savefig("figures/issue2.pdf")
+plt.savefig(snakemake.output[0])
