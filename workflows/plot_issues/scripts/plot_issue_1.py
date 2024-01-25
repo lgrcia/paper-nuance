c8b65feaf7310b188d04ee3612dde5c30f38bbba
lgrcia <lionel_garcia@live.fr>
2024-01-25 14:50:25 -0500

    fix: snr using cdpp + change big plot layout

--- workflows/plot_issues/scripts/plot_issue_1.py
+++ workflows/plot_issues/scripts/plot_issue_1.py
@@ -1,37 +1,39 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import jax
+
 jax.config.update("jax_enable_x64", True)
 import matplotlib.pyplot as plt
 import tinygp
+from utils import snr
 
 # Simulation
 # ------------
 from nuance.utils import transit
+
 np.random.seed(43)
 
 fluxes = []
 snrs = []
 
 # transit
-dur = 1/24
-depth = 1e-2
+duration = 1 / 24
+depth = 0.5e-2
 total_time = 1
-time = np.arange(0, total_time, 1/60/24)
-n = dur/np.median(np.diff(time))
-signal = transit(time, time.mean(), dur, depth) + 1.
+time = np.arange(0, total_time, 1 / 60 / 24)
+t0 = time.mean()
+n = duration / np.median(np.diff(time))
+signal = transit(time, t0, duration, depth, c=5000) + 1.0
 
 # white noise
 w = 1.5e-3
 white_noise = np.random.normal(0, w, len(time))
 flux = white_noise + signal
-intransit = signal < (1. - depth/2)
 
 # No systematics
 # --------------
 
-r = np.max([0, np.std(flux[~intransit]) - w])
-SNR = depth/np.sqrt((w**2)/n + r**2)
+SNR = snr(time, flux, t0, duration)
 
 fluxes.append(flux.copy())
 snrs.append(SNR.copy())
@@ -39,13 +41,12 @@ snrs.append(SNR.copy())
 # Systematics
 # -----------
 
-kernel = tinygp.kernels.quasisep.Matern32(dur, depth/5)
-gp = tinygp.GaussianProcess(kernel, time, diag=0., mean=0.)
+kernel = tinygp.kernels.quasisep.SHO(60.0, 0.5, depth / 4)
+gp = tinygp.GaussianProcess(kernel, time, diag=0.0, mean=0.0)
 red_noise = gp.sample(jax.random.PRNGKey(42))
-flux =  white_noise + red_noise + signal
+flux = white_noise + red_noise + signal
 
-r = np.max([0, np.std(flux[~intransit]) - w])
-SNR = depth/np.sqrt((w**2)/n + r**2)
+SNR = snr(time, flux, t0, duration)
 
 fluxes.append(flux.copy())
 snrs.append(SNR.copy())
@@ -53,21 +54,30 @@ snrs.append(SNR.copy())
 # Plot
 # ----
 
-plt.figure(figsize=(6,5))
-offset = 0.03
-xoff = time.max()*1.2
+plt.figure(figsize=(6, 5))
+offset = 0.02
+xoff = time.max() * 1.2
 
-plt.text(xoff, 1.01, f"transit SNR", ha="center", fontsize=14, va="center")
-plt.text(time.mean(), 1.01, f"Light curve", ha="center", fontsize=14, alpha=0.7)
+plt.text(xoff, 1.008, f"transit SNR", ha="center", fontsize=14, va="center")
+plt.text(time.mean(), 1.008, f"Light curve", ha="center", fontsize=14)
 
 plt.plot(time, fluxes[0], ".", c="0.8")
 plt.plot(time, signal, c="k", alpha=0.4, lw=2)
-plt.text(time.mean(), 1-1.8*depth, "white noise + transit", ha="center", fontsize=14)
+plt.text(
+    time.mean(), 1 - 2.2 * depth, "white noise + transit", ha="center", fontsize=14
+)
 plt.text(xoff, 1, f"{snrs[0]:.2f}", ha="center", fontsize=14, va="center")
 
 plt.plot(time, fluxes[1] - offset, ".", c="0.8")
 plt.plot(time, red_noise + 1 - offset, c="C3", alpha=0.5, lw=2)
-plt.text(time.mean(), 1 - offset -1.8*depth, "+ correlated noise", ha="center", fontsize=14, c="C3")
+plt.text(
+    time.mean(),
+    1 - offset - 2.2 * depth,
+    "+ correlated noise",
+    ha="center",
+    fontsize=14,
+    c="C3",
+)
 plt.text(xoff, 1 - offset, f"{snrs[1]:.2f}", ha="center", fontsize=14, va="center")
 
 plt.axis("off")
--- workflows/plot_issues/scripts/plot_issue_2.py
+++ workflows/plot_issues/scripts/plot_issue_2.py
@@ -4,44 +4,11 @@ from nuance.utils import transit
 import tinygp
 import jax
 import matplotlib.pyplot as plt
+
 jax.config.update("jax_enable_x64", True)
 
-import jax.numpy as jnp
-from tinygp import kernels, GaussianProcess
 import matplotlib.pyplot as plt
-
-
-def white_red(time, flux, error, plot=False, mean=0.):
-    init = {
-        "log_diag": 2*np.log(np.mean(error)),
-        "log_scale": np.log(1), # 5 mins
-        "log_amp": np.log(np.std(flux))
-    }
-
-    def build_gp(params):
-        kernel = jnp.exp(params["log_amp"]) * kernels.ExpSquared(jnp.exp(params["log_scale"]))
-        return GaussianProcess(kernel, time, diag=jnp.exp(params["log_diag"]), mean=mean)
-
-    @jax.jit
-    def loss(params):
-        gp = build_gp(params)
-        return -gp.log_probability(flux)
-    
-    opt = jaxopt.ScipyMinimize(fun=loss)
-    params = opt.run(init).params  
-    w = np.sqrt(np.exp(params['log_diag']))
-    
-    if plot:
-        gp = build_gp(params)
-        _, cond = gp.condition(y=flux, X_test=time)
-        mu = cond.mean
-        plt.plot(time, flux, ".", c="0.8")
-        plt.plot(time, mu, c="k")
-        noise = w
-        plt.fill_between(time, mu + noise, mu - noise, color="C0", alpha=0.3)
-
-    return w, np.sqrt(np.exp(params['log_amp']))
-
+from utils import snr
 
 seed = 42
 
@@ -49,16 +16,20 @@ seed = 42
 # --------------------
 np.random.seed(seed)
 # transit
-time = np.linspace(0, 3, 1000)
-dur = 0.1
+time = np.linspace(-1.5, 1.5, 1000)
+duration = 0.1
 depth = 0.8e-2
-n = dur/np.median(np.diff(time))
-signal = transit(time, time.mean(), dur, depth, c=10) + 1.
+n = duration / np.median(np.diff(time))
+t0 = 0.0
+n = duration / np.median(np.diff(time))
+
+signal = transit(time, time.mean(), duration, depth, c=5000) + 1.0
 
 # white noise
 wsigma = 1.5e-3
 white_noise = np.random.normal(0, wsigma, len(time))
-flux = white_noise + signal
+original_flux = white_noise + signal
+original_snr = snr(time, original_flux, t0, duration)
 
 # Simulate variability
 # --------------------
@@ -67,17 +38,17 @@ variabilities = []
 gps = []
 cond_gps = []
 
-periods = [1000., 1., 0.2, 0.1, 0.04]
-amps = [0., 1e-2, 1e-2, 1e-2, 5e-3]
+periods = [1000.0, 1.0, 0.2, 0.1, 0.04]
+amps = [0.0, 0.8e-2, 0.8e-2, 0.8e-2, 0.8e-2]
 
 for i, (period, amp) in enumerate(zip(periods, amps)):
-    Q = 10.
-    w0 = 1/period
+    Q = 10.0
+    w0 = 1 / period
     S0 = amp
 
     var_kernel = tinygp.kernels.quasisep.SHO(w0, Q, S0)
-    gp = tinygp.GaussianProcess(var_kernel, time, mean=0.)
-    cond_gp = tinygp.GaussianProcess(var_kernel, time, mean=0., diag=wsigma**2)
+    gp = tinygp.GaussianProcess(var_kernel, time, mean=0.0)
+    cond_gp = tinygp.GaussianProcess(var_kernel, time, mean=0.0, diag=wsigma**2)
     variability = gp.sample(jax.random.PRNGKey(seed))
     variability -= np.median(variability)
     flux = white_noise + signal + variability
@@ -92,86 +63,136 @@ wotan_trend_snr = []
 gp_trend_snr = []
 
 cut = 200
-intransit = signal < (1. - depth/2)
+intransit = signal < (1.0 - depth / 2)
 
 for i, (flux, var, gp) in enumerate(zip(lightcurves, variabilities, cond_gps)):
-     # WOTAN
-    flatten_flux, flatten_trend = flatten(time, flux, window_length=3*dur, return_trend=True)
-    new_depth = np.max([0, np.mean(flatten_flux[~intransit][cut:-cut]) - np.mean(flatten_flux[intransit])])
-    r = np.max([0, np.std(flatten_flux[~intransit]) - wsigma])
-    SNR = new_depth/np.sqrt((wsigma**2)/n + r**2)
+    # WOTAN
+    flatten_flux, flatten_trend = flatten(
+        time, flux, window_length=3 * duration, return_trend=True
+    )
+    SNR = snr(time, flatten_flux, t0, duration)
     wotan_trend_snr.append((flatten_trend, SNR))
-    
-    #GP
-    _, cond = gp.condition(y=flux-1., X_test=time)
-    variability_model = cond.mean + 1.
-    flatten_flux = flux - variability_model + 1.
-    new_depth = np.max([0, np.mean(flatten_flux[~intransit][cut:-cut]) - np.mean(flatten_flux[intransit])])
-    r = np.max([0, np.std(flatten_flux[~intransit]) - wsigma])
-    SNR = new_depth/np.sqrt((wsigma**2)/n + r**2)
+
+    # GP
+    _, cond = gp.condition(y=flux - 1.0, X_test=time)
+    variability_model = cond.mean + 1.0
+    flatten_flux = flux - variability_model + 1.0
+    SNR = snr(time, flatten_flux, t0, duration)
     gp_trend_snr.append((variability_model, SNR))
 
 
 # Plots
 # -----
 offset = 0.05
-plt.figure(figsize=(10,7))
 
 wotan_color = "C4"
 gp_color = "C3"
 
-intransit = signal < (1. - depth/2)
+intransit = signal < (1.0 - depth / 2)
 
 x_wotan = 3
 x_gp = 6.2
 
 snr_offset = 0.6
 
+from matplotlib.pyplot import GridSpec
+
+fig = plt.figure(figsize=(8.5, 6))
+gs = GridSpec(5, 5, figure=fig, width_ratios=[1, 0.6, 0.2, 0.6, 0.2], hspace=0)
+
+ylim = (0.96, 1.03)
+
 for i, (flux, var, gp) in enumerate(zip(lightcurves, variabilities, gps)):
-    plt.plot(time, flux - offset*i, ".", c="0.8")
-    if i==0:
-        plt.plot(time, signal - offset*i, c="k", alpha=0.5, lw=2)
-    
+    ax = plt.subplot(gs[i, 0])
+    ax_wotan = plt.subplot(gs[i, 1])
+    ax_gp = plt.subplot(gs[i, 3])
+    ax.plot(time, flux, ".", c="0.8")
+    if i == 0:
+        ax.plot(time, signal, c="k", alpha=0.5, lw=2)
+
     # wotan
-    trend, snr = wotan_trend_snr[i]
-    if i!=0:
-        plt.plot(time, trend - offset*i, c=wotan_color, alpha=0.6, lw=2)
-        plt.text(time[-cut] + x_wotan + snr_offset, 1 - i*offset, f"{snr:.2f}", ha="center", fontsize=13)
-        plt.plot(time[cut:-cut] + x_wotan, (flux-trend)[cut:-cut] - offset*i + 1., ".", c="0.8"), 
-    
+    trend, _snr = wotan_trend_snr[i]
+    if i != 0:
+        ax.plot(time, trend, c=wotan_color, alpha=0.6, lw=2)
+        plt.subplot(gs[i, 2]).text(0.5, 0.5, f"{_snr:.2f}", ha="center", fontsize=12)
+        ax_wotan.plot(
+            time,
+            (flux - trend) + 1.0,
+            ".",
+            c="0.8",
+        ),
+
     # gp
-    trend, snr = gp_trend_snr[i]
-    if i!=0:
-        plt.plot(time, trend - offset*i, c=gp_color, alpha=0.6, lw=2)
-        plt.text(time[-cut] + x_gp + snr_offset, 1 - i*offset, f"{snr:.2f}", ha="center", fontsize=13)
-        plt.plot(time[cut:-cut] + x_gp, (flux-trend)[cut:-cut] - offset*i + 1., ".", c="0.8")
+    trend, _snr = gp_trend_snr[i]
+    if i != 0:
+        ax.plot(time, trend, c=gp_color, alpha=0.6, lw=2)
+        plt.subplot(gs[i, 4]).text(0.5, 0.5, f"{_snr:.2f}", ha="center", fontsize=12)
+        ax_gp.plot(
+            time,
+            (flux - trend) + 1.0,
+            ".",
+            c="0.8",
+        )
 
-    if i!=0:
-        plt.plot(time, var + 1 - offset*i, c="C0", alpha=0.6, lw=2)
+    if i != 0:
+        ax.plot(time, var + 1, c="C0", alpha=0.6, lw=2)
 
+    ax.set_ylim(ylim)
+    ax_wotan.set_ylim(ylim)
+    ax_gp.set_ylim(ylim)
 
-txt_color = "0.2"
-    
-plt.text(time.mean(), 1.015, "original light curve", ha="center", fontsize=14, color=txt_color)
-plt.text(time.min(), 1.02 - offset, "+ variability", ha="left", fontsize=14, alpha=1, c="C0")
+    xlim = np.array([-1, 1]) * 0.6 + t0
+    ax_wotan.set_xlim(xlim)
+    ax_gp.set_xlim(xlim)
 
-OFFSET = 1
+    if i != 0:
+        ax.text(
+            -1.5,
+            1.03,
+            f"$\omega = ${2*np.pi/periods[i]:.0f}",
+            ha="left",
+            fontsize=11,
+            color="0.5",
+        )
 
-# wotan txt
-def text(x, method, color):
-    xrange = np.array([0, 2.5]) + x + 0.7
-    plt.text(xrange.mean(), OFFSET-0.01, method, ha="center", fontsize=15, c=color)
-    plt.plot(xrange + 0.1, (OFFSET-0.015, OFFSET-0.015), "-", c=color, lw=2)
-    plt.text(time[-cut] + x + snr_offset, OFFSET-0.03, "SNR", ha="center", fontsize=14, c=txt_color)
-    plt.text(time[cut:-cut].mean() + x, OFFSET-0.03, "cleaned light curve", ha="center", fontsize=14, c=txt_color)
+for i in range(0, 4):
+    for j in [0, 1, 3]:
+        ax = plt.subplot(gs[i, j])
+        ax.axis("off")
 
-text(x_wotan, "a. biweight", wotan_color)
-text(x_gp, "b. SHO GP", gp_color)
+for i in range(0, 5):
+    for j in [2, 4]:
+        ax = plt.subplot(gs[i, j])
+        ax.axis("off")
 
-original_snr = depth/np.sqrt(wsigma**2/n)
-plt.text(6.5, 1.015, "original SNR", ha="center", fontsize=14, color=txt_color)
-plt.text(6.5, 1.005, f"{original_snr:.2f}", ha="center", fontsize=14, color=txt_color)
+for i in [0, 1, 3]:
+    ax = plt.subplot(gs[-1, i])
+    ax.set_yticks([])
+    ax.spines["right"].set_visible(False)
+    ax.spines["top"].set_visible(False)
+    ax.spines["left"].set_visible(False)
 
-plt.axis("off")
+plt.subplot(gs[0, 0]).set_title("original light curve", fontsize=12)
+plt.subplot(gs[1, 1]).set_title("cleaned with bi-weight", color="C4", fontsize=12)
+plt.subplot(gs[1, 3]).set_title("cleaned with GP", color="C3", fontsize=12)
+plt.subplot(gs[1, 2]).set_title("SNR", fontsize=12)
+plt.subplot(gs[1, 4]).set_title("SNR", fontsize=12)
+
+plt.subplot(gs[-1, 0]).set_xlabel("time (days)")
+plt.subplot(gs[-1, 1]).set_xlabel("time (days)")
+plt.subplot(gs[-1, 3]).set_xlabel("time (days)")
+
+top_ax = plt.subplot(gs[0:1, 1:5])
+top_ax.axis("off")
+top_ax.text(
+    0.5,
+    1,
+    f"original SNR\n{original_snr:.2f}",
+    ha="center",
+    va="top",
+    fontsize=12,
+    linespacing=1.5,
+)
+
 plt.tight_layout()
 plt.savefig(snakemake.output[0])
